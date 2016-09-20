c5bcab1e5d33b73d7bf57d916a14ab4600b245ee
Hitoshi Manabe <hitoshi-ma@dn156-045.naist.jp>
2016-09-20 18:11:08 +0900

    add pos_eval.py

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # maeval
 evaluation script for morphological analysis
+
+### run
+`python ws_eval.py --pred /path/to/pred_file --gold /path/to/gold_file`
--- pos_eval.py
+++ pos_eval.py
@@ -1,43 +1,51 @@
-'''
-calcurating F-measure score for evaluation word segmentation
+"""
+This script is calcurate precision, recall and F-score for Japanese
+Morphological Analysis
 
 TODO
 * loop in evaluation function can be made more easy
-'''
+"""
 
 
 import argparse
 
 
-def transform(sent):
+def transform(sent, separator):
     """
     Args:
-            sent : word-segmented sentence, and at each word POS is attached
-              (e.g "私 は 大学生 です")
+            sent : word segmented sentence, and at each word POS is attached
+                (e.g "私_名詞 は_助詞 大学生_名詞")
+            separator : separator between word and POS tag
     Return:
-            posits : words in sent are substituted into positions in sent
-              (e.g "[1:1, 2:2, 3:5, 6:7]")
+            pais : words in sent are substituted into positions in sent
+                (e.g "1:1_名詞 2:2_助詞 3:5_名詞")
     """
-    word_list = sent.split(' ')
-    posits = []
+    wordpos_list = [wordpos.split(separator) for wordpos in sent.split(' ')]
+    pairs = []
     start = 1
-    for word in word_list:
+    for word, pos in wordpos_list:
         end = start + len(word) - 1
         # change word into position
-        posits.append('{}:{}'.format(start, end))
+        pairs.append('{}:{}{}{}'.format(start, end, separator, pos))
         start = end + 1
-    return posits
+    return pairs
 
 
-def evaluation(gold_file, pred_file):
+def check_pair(sent1, sent2, separator):
+    words1 = [wordpos.split(separator)[0] for wordpos in sent1.split(' ')]
+    words2 = [wordpos.split(separator)[0] for wordpos in sent2.split(' ')]
+    return ''.join(words1) == ''.join(words2)
+
+
+def evaluation(gold_file, pred_file, separator="_"):
     with open(gold_file) as fg, open(pred_file) as fp:
         n_correct = 0
         n_goldw = 0
         n_predw = 0
         for g_sent, p_sent in zip(fg, fp):
-            assert g_sent.replace(' ', '') == p_sent.replace(' ', '')
-            gold_list = transform(g_sent.strip('\n'))
-            pred_list = transform(p_sent.strip('\n'))
+            assert check_pair(g_sent.strip('\n'), p_sent.strip('\n'), separator)
+            gold_list = transform(g_sent.strip('\n'), separator)
+            pred_list = transform(p_sent.strip('\n'), separator)
             n_goldw += len(gold_list)
             n_predw += len(pred_list)
             # calcurating the number of words correctly predicted
